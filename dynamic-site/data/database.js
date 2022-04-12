87ac55ec2f35aa054fed0a725a0a2f915db7dad8
arpitmoliya <arpitmoliya153@gmail.com>
2022-04-12 10:10:54 +0530

    change the path of database server

--- dynamic-site/data/database.js
+++ dynamic-site/data/database.js
@@ -1,17 +1,17 @@
-const mongodb = require('mongodb');
+const mongodb = require("mongodb");
 
 const MongoClient = mongodb.MongoClient;
 
 let database;
 
 async function initDatabase() {
-  const client = await MongoClient.connect('mongodb://localhost:27017');
-  database = client.db('deployment');
+  const client = await MongoClient.connect("mongodb://127.0.0.1:27017");
+  database = client.db("deployment");
 }
 
 function getDb() {
   if (!database) {
-    throw new Error('No database connected!');
+    throw new Error("No database connected!");
   }
 
   return database;
